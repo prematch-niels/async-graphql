6fcb5efc21c30a42f9d2883075662f7d632ed9df
Niels <niels@prematchapp.de>
2023-10-27 14:08:44 +0200

    switched json -> bincode

--- integrations/axum/src/response.rs
+++ integrations/axum/src/response.rs
@@ -27,7 +27,7 @@ impl From<async_graphql::BatchResponse> for GraphQLResponse {
 impl IntoResponse for GraphQLResponse {
     fn into_response(self) -> Response<BoxBody> {
         // let body: Body = serde_json::to_string(&self.0).unwrap().into();
-        let body = Full::new(bincode::serialize(&self.0).unwrap());
+        let body = Full::new(bincode::serialize(&self.0).unwrap().into());
         let mut resp = Response::new(body);
         resp.headers_mut().insert(
             http::header::CONTENT_TYPE,
